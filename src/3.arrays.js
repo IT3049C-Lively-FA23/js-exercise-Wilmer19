d9a91f54d5468a18d9a9f9085c38903d7212a4a2
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-09-03 15:58:34 -0400

    removing last item from array method

--- src/3.arrays.js
+++ src/3.arrays.js
@@ -29,7 +29,7 @@ function append(arr, item) {
 // Truncate: you should be able to remove the last item of an array
 // hint: pop, slice, splice would all work. Pay attention to the return value in the docs
 function truncate(arr) {
-  return ;
+  return arr.slice(0, -1);
 }
 
 // Concat: you should be able to join together two arrays
