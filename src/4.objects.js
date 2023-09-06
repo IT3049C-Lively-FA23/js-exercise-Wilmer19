bcd22dc1f57bb36c03d003e3765a657e3ce97b31
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-09-05 21:08:32 -0400

    Object method

--- src/4.objects.js
+++ src/4.objects.js
@@ -8,8 +8,41 @@
  *      * must call the `full_name()` function to present the person's name.
  *      * must use the `class_of` property in the introduction
  */
+/*
 const personObject = {
+  first_Name: 'Wilmer',
+  last_Name : 'Esquivel',
+  class_of: 2024,
+  full_Name : function() {
+    console.log(this.first_Name + " " + this.last_Name);
+  }
+};
 
+console.log(personObject.first_Name);
+console.log(personObject.last_Name);
+personObject.full_Name();
+//personObject.introduction();
+
+function introduction(){
+  console.log('Welcome! My name is ' + personObject.full_Name + ' and I am the class of ' + personObject.class_of + '.');
+}
+introduction();
+
+//};
+//console.log(personObject.full_Name());
+//console.log(personObject.introduction());
+*/
+
+const personObject = {
+  first_Name: "Wilmer",
+  last_Name : "Esquivel",
+  class_of: 2024,
+  full_Name : function() {
+    return this.first_Name + " " + this.last_Name;
+  },
+  introduction : function(full_Name) {
+      return "Welcome! My name is " + full_Name() + " and I'm in the class of " + personObject.class_of + ".";
+  }
 };
 
 module.exports = {
